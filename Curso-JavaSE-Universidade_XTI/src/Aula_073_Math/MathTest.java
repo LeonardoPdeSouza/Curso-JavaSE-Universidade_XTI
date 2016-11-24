f1fbfa35c6ccabaf8c015755ecdb13416c7872a3
leonardops <leonardops@leonardops-Aspire-E5-573G>
2016-11-24 14:54:44 -0200

    Universidade XTI - JAVA - 073 - Math

---
+++ Curso-JavaSE-Universidade_XTI/src/Aula_073_Math/MathTest.java
@@ -0,0 +1,16 @@
+package Aula_073_Math;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		System.out.println(Math.sin(10)); // Seno
+		System.out.println(Math.cos(10)); // Cosseno
+		System.out.println(Math.tan(45)); // Tangente
+		System.out.println(Math.pow(4, 3)); // Potência
+		System.out.println(Math.sqrt(16)); // Raiz quadrada
+		System.out.println(Math.cbrt(16)); // Raiz cúbica
+		System.out.println(Math.hypot(6, 8)); // Hypotenusa
+
+	}
+
+}
